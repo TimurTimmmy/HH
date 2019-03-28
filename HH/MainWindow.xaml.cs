6f2cdf9e82f1c46c2a388d776834c7c8dc7bb2e6
Timur <ash123@mail.ru>
2019-03-28 08:41:07 +0300

    Добавьте файлы проекта.

---
+++ HH/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json.Linq;
+
+namespace HH
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+        /// Выводить кол-во страниц по радиобатону
+        /// Работадатель
+        /// Координаты, метро, подсветка по цвету линии метро
+        /// ссылка на вакансию
+        /// Поситчать среднюю ЗП
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public int pages;
+        public int curpage;
+        public int found;
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string req = SearchText.Text;
+            string url = string.Format(@"https://api.hh.ru/vacancies?per_page=100&page={1}&text={0}", req, curpage);
+            LoadResult(Get_http(url));
+            MessageBox.Show("Найдено " + found + " вакансий.", "Результат");
+        }
+        public string Get_http(string url)
+        {
+            CookieContainer cookies = new CookieContainer();
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);            
+            req.UserAgent = "ash123@mail.ru";
+            req.CookieContainer = cookies;
+            req.Headers.Add("DNT", "1");
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string result = sr.ReadToEnd();
+            resp.Close();
+            sr.Close();
+            return result;
+        }
+
+        public void LoadResult(string jstring)
+        {
+            List<Vacancy> VacList = new List<Vacancy>();
+            JObject VacObj = JObject.Parse(jstring);
+            pages = (int)VacObj["pages"];
+            curpage = (int)VacObj["page"];
+            found = (int)VacObj["found"];
+            var items = from jVac in VacObj["items"]
+                        select new Vacancy((string)jVac["id"], (string)jVac["name"], (string)jVac["salary"]["from"], (string)jVac["salary"]["to"], 
+                        (string)jVac["snippet"]["requirement"]+ "\n" + (string)jVac["snippet"]["responsibility"] );
+            foreach (var it in items) { VacList.Add(it); }
+            ResultGrid.SelectionChanged -= ResultGrid_SelectionChanged;
+            ResultGrid.ItemsSource = VacList;
+            ResultGrid.SelectionChanged += ResultGrid_SelectionChanged;
+            LabelPages.Content = string.Format("Страница {0}", curpage);
+        }
+
+        private void ResultGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            DataGrid vac = (DataGrid)sender;
+            Vacancy VacId = (Vacancy)vac.SelectedValue;
+            TbDescryption.Text = "Краткое описание: \n" + VacId.Description;
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {            
+            if (curpage < pages - 1)
+            {                
+                curpage += 1;
+                LabelPages.Content = string.Format("Страница {0}", curpage);
+                string req = SearchText.Text;
+                string url = string.Format(@"https://api.hh.ru/vacancies?per_page=100&page={1}&text={0}", req, curpage);
+                LoadResult(Get_http(url));
+            }
+        }   
+    }
+}
+
+
---
+++ HH/Vacancy.cs
@@ -0,0 +1,55 @@
+﻿
+namespace HH
+{
+    public class Vacancy
+    {
+        private string id;
+        private string name;
+        private string description;
+        private string minsalary;
+        private string maxsalary;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string MinSalary
+        {
+            get { return minsalary; }
+            set { minsalary = value; }
+        }
+        public string MaxSalary
+        {
+            get { return maxsalary; }
+            set { maxsalary = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public Vacancy(string Id, string Name, string MinSalary, string MaxSalary, string Description)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.MinSalary = MinSalary;
+            this.MaxSalary = MaxSalary;
+            this.Description = Description;
+        }
+
+        public Vacancy(string Id, string Name, string Description)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.MinSalary = "не указано";
+            this.MaxSalary = "не указано";
+            this.Description = Description;
+        }
+    }
+}
